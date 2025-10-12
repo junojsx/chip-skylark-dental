7db9ebcead5a74b70217399cfe8cfb4bbae3d542
junojsx <junojsx@gmail.com>
2025-10-11 23:05:08 -0700

    setting up clerk

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className="">
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <Button>Click me</Button>
+      <SignUpButton> Sign Up</SignUpButton>
     </div>
   );
 }
