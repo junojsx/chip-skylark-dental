d1a07223680c321e0822aba4fe76a68cc73d8b09
junojsx <junojsx@gmail.com>
2025-10-14 11:27:23 -0700

    initializing how it works page

--- src/components/landing/HowItWorks.tsx
+++ src/components/landing/HowItWorks.tsx
@@ -1,8 +1,16 @@
+import { ZapIcon } from "lucide-react";
+
 const HowItWorks = () => {
   return (
-    <div className="">
-      <h1>HowItWorks</h1>
-    </div>
+    <section className=" relative py-32 px-6 outline-hidden z-10 mx-auto max-w-7xl">
+      {/* Header Section */}
+      <div className="text-center">
+         <div className="inline-flex items-center gap-2 px-4 py-2 bg-gradient-to-r from-primary/5 to-primary/10 rounded-full border border-primary/10 backdrop-blur-sm mb-6">
+          <ZapIcon className="size-4 text-primary" />
+          <span className="text-sm font-medium text-primary">Simple Process</span>
+        </div>
+      </div>
+    </section>
   );
 };
 
